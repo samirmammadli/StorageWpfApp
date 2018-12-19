131cdd759bcd1e85ad637ef7912a8bdc0476fbf4
Samir Mammadli <mammadli.s86@gmail.com>
2018-12-19 11:29:43 +0400

    1

---
+++ StorageWpfApp/Converters/NameAndLastNameConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace StorageWpfApp.Converters
+{
+    class NameAndLastNameConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                return (values[0] as string) + " " + (values[1] as string);
+            }
+            catch (Exception)
+            {
+
+                
+            }
+
+            return "";
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
